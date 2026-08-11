f745dfc132f4901b71669f99c7d6cd85287d9f7c
Ezekiel Sanmabo <eomoobamidele@gmail.com>
2026-08-11 09:17:11 +0100

    update dag

--- airflow/dags/fstdag.py
+++ airflow/dags/fstdag.py
@@ -11,17 +11,17 @@ with DAG(
 ) as dag:
     install_deps = BashOperator(
         task_id='dbt_deps',
-        bash_command='cd /opt/airflow/dbt && dbt deps',
+        bash_command='cd /opt/sql/qntplatform && dbt deps',
     )
 
     run_dbt = BashOperator(
         task_id='dbt_run',
-        bash_command='cd /opt/airflow/dbt && dbt run --profiles-dir /opt/airflow/dbt',
+        bash_command='cd /opt/sql/qntplatform && dbt run --profiles-dir /opt/sql/qntplatform',
     )
 
     test_dbt = BashOperator(
         task_id='dbt_test',
-        bash_command='cd /opt/airflow/dbt && dbt test --profiles-dir /opt/airflow/dbt',
+        bash_command='cd /opt/sql/qntplatform && dbt test --profiles-dir /opt/sql/qntplatform',
     )
 
     install_deps >> run_dbt >> test_dbt
